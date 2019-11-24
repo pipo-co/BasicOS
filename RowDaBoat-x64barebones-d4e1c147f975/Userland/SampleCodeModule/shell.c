699cd07f9e0f91045a5e021fd3c6eee79d5fbd1a
Ignacio Sagues <fpannunzio@itba.edu.ar>
2019-11-24 11:08:56 -0300

    Musics

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/shell.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/shell.c
@@ -2,7 +2,7 @@
 #include <usrlib.h>
 
 #define USER_INPUT_SIZE 50
-#define MAX_FUNCTIONS 10
+#define MAX_FUNCTIONS 12
 #define MAX_ARGUMENTS_SIZE 5
 #define ESC 27
 #define CURSOR_COLOR 0x00FF00
@@ -39,6 +39,8 @@ enum chords{A=880, AS= 932, B=988, C=523, CS=554, D=587, DS=622, E=659, F=698, F
     static int triggerException6(int argcount, char * args[]);
     static int playSound(int argcount, char * args[]);
     static int playMusic(int argcount, char * args[]);
+    static int forElisa(int argcount, char * args[]);
+    static int Victory(int argcount, char * args[]);
     static void turnOffCursor();
     static void tickCursor();
 //End
@@ -121,7 +123,9 @@ static void loadFunctions(){
     loadFunction("triggerException0",&triggerException0, "Triggers Exception number 0 \n");
     loadFunction("triggerException6",&triggerException6, "Triggers Exception number 6 \n");
     loadFunction("beep",&playSound, "Plays a beep \n");
-    loadFunction("Music",&playMusic, "Plays some music \n");
+    loadFunction("Lavander",&playMusic, "Plays an indie game's music\n");
+    loadFunction("Elisa", &forElisa, "Music for a student \n");
+    loadFunction("Music", &Victory, "Music for a student \n");
 }
 
 static void loadFunction(char * string, int (*fn)(), char * desc){
@@ -267,6 +271,87 @@ static int triggerException6(int argcount, char * args[]){
 static int playSound(int argcount, char * args[]){
     sysBeep(1000,5);
     return 0;
+}
+static int Victory(int argcount, char * args[]){
+    sysBeep(B/2,4);
+    sysBeep(A/2,4);
+    sysBeep(E/4,16);
+    sysBeep(A/2,4);
+    sysBeep(C,4);
+    sysBeep(D,8);
+    sysBeep(A/2,24);
+    sysBeep(C,16);
+    sysBeep(B/2,4);
+    sysBeep(C,4);
+    sysBeep(D,4);
+    sysBeep(G/2,4);
+    sysBeep(E,16);
+    sysBeep(D,16);
+
+    sysBeep(B/2,4);
+    sysBeep(A/2,4);
+    sysBeep(E/4,16);
+    sysBeep(A/2,4);
+    sysBeep(C,4);
+    sysBeep(D,8);
+    sysBeep(A/2,24);
+    sysBeep(C,16);
+    sysBeep(B/2,4);
+    sysBeep(C,4);
+    sysBeep(D,4);
+    sysBeep(G/2,4);
+    sysBeep(A/2,32);
+
+
+    
+}
+
+static int forElisa(int argcount, char * args[]){
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(E/2,6);
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(B/2,6);
+    sysBeep(DS,6);
+    sysBeep(C,6);
+    sysBeep(A/2,12);
+    sysBeep(C/2,6);
+    sysBeep(E/2,6);
+    sysBeep(A/2,6);
+    sysBeep(B/2,12);
+    sysBeep(E/2,6);
+    sysBeep(GS/2,6);
+    sysBeep(B/2,6);
+    sysBeep(C,12);
+
+    sysBeep(E/2,6);
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(DS,6);
+    sysBeep(E,6);
+    sysBeep(B/2,6);
+    sysBeep(DS,6);
+    sysBeep(C,6);
+    sysBeep(A/2,12);
+    sysBeep(C/2,6);
+    sysBeep(E/2,6);
+    sysBeep(A/2,6);
+    sysBeep(B/2,12);
+    sysBeep(E/2,6);
+    sysBeep(C,6);
+    sysBeep(B/2,6);
+    sysBeep(A/2,12);
+    
+    
+    
 }
 
 static int playMusic(int argcount, char * args[]){
