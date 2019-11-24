86f3826415577ea9d9ef8df84b69b9fa39b2391d
Tobias Brandy <tobiasbrandy@hotmail.com>
2019-11-24 18:06:47 -0300

    Exception

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/interrupts/exceptions.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/interrupts/exceptions.c
@@ -9,18 +9,18 @@
 
 static void zero_division(uint64_t * exceptionStackframe);
 static void invalid_opcode(uint64_t * exceptionStackframe);
+static void restartSampleCodeModule(uint64_t * exceptionStackframe);
+static void printRegs(uint64_t * address);
 
 static uint64_t continueExecutionIP, continueExecutionSP;
 
-char * regs[] = {
+static char * regs[] = {
 	"R15: ", "R14: ", "R13: ", "R12: ", "R11: ", "R10: ", "R9: ",
 	"R8: ", "RSI: ", "RDI: ", "RBP: ", "RDX: ", "RCX: ", "RBX: ",
 	"RAX: ", "IP: ", "RSP: "
 };
 
-char buffer[5];
-
-void printRegs(uint64_t * address){
+static void printRegs(uint64_t * address){
 	char buffer[50];
 	for (int i = 0; i < 16; i++){
 		printString(regs[i]);
@@ -30,6 +30,33 @@ void printRegs(uint64_t * address){
 	printString(regs[16]);
 	uintToBase((uint64_t)(address + 15 + 3), buffer, 16); //RSP
 	println(buffer);
+}
+
+void initExceptionHandler(uint64_t ipAdress, uint64_t stackAdress){
+	continueExecutionIP = ipAdress;
+	continueExecutionSP = stackAdress;
+}
+
+static void restartSampleCodeModule(uint64_t * exceptionStackframe){
+	int aux;
+	char buffer[5];
+
+	exceptionStackframe[15] = continueExecutionIP;
+	exceptionStackframe[15+3] = continueExecutionSP;
+
+	printString("Restarting shell in ");
+
+	for (int i = 60; i > 0; i--){
+		uintToBase(i, buffer, 10);
+		printString(buffer);
+
+		_hlt();
+
+		aux = strilen(buffer);
+
+		for (int i = 0; i < aux; i++)
+			putchar('\b');
+	}
 }
 
 void exceptionDispatcher(int exception, uint64_t * exceptionStackframe) {
@@ -46,30 +73,11 @@ void exceptionDispatcher(int exception, uint64_t * exceptionStackframe) {
 static void zero_division(uint64_t * exceptionStackframe){
 	println("Exception 0: Division by 0");
 	printRegs(exceptionStackframe);
-	exceptionStackframe[15] = continueExecutionIP;
-	exceptionStackframe[15+3] = continueExecutionSP;
-	printString("Restarting shell in ");
-	int aux;
-	for (int i = 60; i > 0; i--){
-		uintToBase(i, buffer, 10);
-		printString(buffer);
-		_hlt();
-		aux = strilen(buffer);
-		for (int i = 0; i < aux; i++)
-			putchar('\b');
-	}
-	
+	restartSampleCodeModule(exceptionStackframe);
 }
 
 static void invalid_opcode(uint64_t * exceptionStackframe){
 	println("Exception 6: Invalid Opcode");
 	printRegs(exceptionStackframe);
-	while(1); //????
-}
-
-void initExceptionHandler(uint64_t ipAdress, uint64_t stackAdress){
-	continueExecutionIP = ipAdress;
-	continueExecutionSP = stackAdress;
-
-	
+	restartSampleCodeModule(exceptionStackframe);
 }
