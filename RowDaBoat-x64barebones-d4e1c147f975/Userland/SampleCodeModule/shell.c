28f18448d1f0fdf792431b189ad9bb34d21abfdd
igvazquez <igvazquez@itba.edu.ar>
2019-11-24 12:54:25 -0300

    Merge conflicts 2.0

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/shell.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/shell.c
@@ -353,6 +353,37 @@ static int Defeat(int argcount, char * args[]){
     
 }
 
+static int Victory(int argcount, char * args[]){
+    sysBeep(B/2,4);
+    sysBeep(A/2,4);
+    sysBeep(E/4,16);
+    sysBeep(A/2,4);
+    sysBeep(C,4);
+    sysBeep(D,8);
+    sysBeep(A/2,24);
+    sysBeep(C,16);
+    sysBeep(B/2,4);
+    sysBeep(C,4);
+    sysBeep(D,4);
+    sysBeep(G/2,4);
+    sysBeep(E,16);
+    sysBeep(D,16);
+
+    sysBeep(B/2,4);
+    sysBeep(A/2,4);
+    sysBeep(E/4,16);
+    sysBeep(A/2,4);
+    sysBeep(C,4);
+    sysBeep(D,8);
+    sysBeep(A/2,24);
+    sysBeep(C,16);
+    sysBeep(B/2,4);
+    sysBeep(C,4);
+    sysBeep(D,4);
+    sysBeep(G/2,4);
+    sysBeep(A/2,32);
+}
+
 static int forElisa(int argcount, char * args[]){
     sysBeep(E,6);
     sysBeep(DS,6);
